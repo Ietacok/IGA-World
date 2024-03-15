846eca95ad864dc57dbd9f2315babd3473f5fc2f
NotAsExpected <ksztnnnn@gmail.com>
2024-03-15 12:58:06 +0100

    Shop

--- scripts/shop/shop-load-wrap.js
+++ scripts/shop/shop-load-wrap.js
@@ -1,4 +1,21 @@
-function load_shop(total_area) //total_area = rows * cols (col = 25% of the screen) 
+import * as shop_manager from "./shop-manager"
+
+let filter = shop_manager.createFilterOptions();
+
+function set_tiles(total_area)
 {
- let big_images_area = total_area;
+ let tiles_group = [];
+
+ return tiles_group;
 }
+
+function load_shop(total_area) //total_area = rows * (col * 4)
+{
+ let advertised_products_area = Math.floor((total_area / 4) * 0.1); //2x2
+ let other_products = total_area - advertised_products_area; //1x1
+ let tiles_group = set_tiles(total_area);
+
+ 
+}
+
+load_shop(4*15);
